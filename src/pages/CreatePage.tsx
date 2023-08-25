db63b180a07f7ce9a6ce9a0af93e3390271fe88c
eudaldmoya <eudald.moya.8@gmail.com>
2023-08-25 16:01:29 +0200

    Add ListPage and CreatePage with titles to navigate

---
+++ src/pages/CreatePage.tsx
@@ -0,0 +1,11 @@
+const CreatePage = () => {
+  return (
+    <section>
+      <header>
+        <h1>Create a Robot</h1>
+      </header>
+    </section>
+  );
+};
+
+export default CreatePage;
---
+++ src/pages/ListPage.tsx
@@ -0,0 +1,11 @@
+const ListPage = () => {
+  return (
+    <section>
+      <header>
+        <h1>List Page</h1>
+      </header>
+    </section>
+  );
+};
+
+export default ListPage;
