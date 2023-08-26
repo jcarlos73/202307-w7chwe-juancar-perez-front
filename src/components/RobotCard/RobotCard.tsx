2ae2a9b09c120d7d6ff13adc08c0b187276784a6
eudaldmoya <eudald.moya.8@gmail.com>
2023-08-26 20:37:13 +0200

    Style RobotCard component

--- src/components/RobotCard/RobotCard.tsx
+++ src/components/RobotCard/RobotCard.tsx
@@ -1,20 +1,35 @@
 import { Robot } from "../../types";
+import "./RobotCard.css";
 
 interface RobotCardProps {
   robot: Robot;
 }
 
-const RobotCard = ({ robot: { name, imageUrl } }: RobotCardProps) => {
+const RobotCard = ({
+  robot: { name, imageUrl, resistance, speed },
+}: RobotCardProps) => {
   return (
     <article className="card">
       <img src={imageUrl} alt={`Portrait of ${name}`} className="card__img" />
       <div className="card__info">
         <h2 className="card__name">{name}</h2>
-        <div className="card__speed bar">
-          <div className="bar__inner"></div>
+        <div>
+          <span>Speed</span>
+          <div className="card__speed bar">
+            <div
+              className="bar__inner"
+              style={{ width: `${(speed / 10) * 100}%` }}
+            ></div>
+          </div>
         </div>
-        <div className="card__resistance bar">
-          <div className="bar__inner"></div>
+        <div>
+          <span>Resistance</span>
+          <div className="card__resistance bar">
+            <div
+              className="bar__inner"
+              style={{ width: `${(resistance / 10) * 100}%` }}
+            ></div>
+          </div>
         </div>
       </div>
     </article>
