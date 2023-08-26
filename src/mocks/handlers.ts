3c39ee53688182ca618a263e4338e6018ed76bb0
eudaldmoya <eudald.moya.8@gmail.com>
2023-08-26 18:07:57 +0200

    Set up msw and get handler

---
+++ src/mocks/handlers.ts
@@ -0,0 +1,11 @@
+import { rest } from "msw";
+import { robotsMock } from "./robotsMock";
+
+export const handlers = [
+  rest.get(
+    `${import.meta.env.VITE_ROBOTS_API_URL}/robots`,
+    (_req, res, ctx) => {
+      return res(ctx.status(200), ctx.json(robotsMock));
+    },
+  ),
+];
