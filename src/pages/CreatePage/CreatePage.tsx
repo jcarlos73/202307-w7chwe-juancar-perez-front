43f39c752e81bb8e3d69f2a012f00790c6b7f377
eudaldmoya <eudald.moya.8@gmail.com>
2023-08-27 16:28:26 +0200

    Style new robot form

--- src/pages/CreatePage/CreatePage.tsx
+++ src/pages/CreatePage/CreatePage.tsx
@@ -1,9 +1,13 @@
+import CreateRobotForm from "../../components/CreateRobotForm/CreateRobotForm";
+import "./CreatePage.css";
+
 const CreatePage = () => {
   return (
-    <section>
+    <section className="form-section">
       <header>
         <h2>Create a Robot</h2>
       </header>
+      <CreateRobotForm />
     </section>
   );
 };
