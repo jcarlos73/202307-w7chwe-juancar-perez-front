d0b78c90a4979bd5fccca73893993fbb59d38dae
Juan Carlos Pérez <jcparetas@gmail.com>
2023-08-28 16:32:34 +0200

    clone repo

--- src/pages/ListPage/ListPage.tsx
+++ src/pages/ListPage/ListPage.tsx
@@ -1,12 +1,31 @@
+import { signInWithPopup, signOut } from "firebase/auth";
+import { useAuthState } from "react-firebase-hooks/auth";
 import RobotList from "../../components/RobotList/RobotList";
+import { auth, githubProvider } from "../../firebaseConfig";
 
-const ListPage = () => {
+const ListPage = (): React.ReactElement => {
+  const [user, isLoading] = useAuthState(auth);
+
+  if (isLoading) {
+    return <h2>Loading...</h2>;
+  }
+
+  const login = async () => {
+    await signInWithPopup(auth, githubProvider);
+  };
+
+  const logout = async () => {
+    await signOut(auth);
+  };
+
   return (
     <section>
       <header>
         <h2>List Page</h2>
+        <button onClick={login}>Login</button>
+        <button onClick={logout}>Logout</button>
       </header>
-      <RobotList />
+      {user && <RobotList />}
     </section>
   );
 };
